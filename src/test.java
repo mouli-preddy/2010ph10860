3e43455be2ea8753cf54b01426283474fb3451bf
mouli <mouli@eggheadcreative.com>
2013-09-12 15:51:49 +0530

    adding test.java to gitignore

--- src/test.java
+++ src/test.java
@@ -5,9 +5,6 @@
  * Submission date: 15th September, 2013
  */
 public class test {
-  public static void main(String ar[]) {
-    String test = "";
-    if (test.length() > 1)
-      System.out.println("The string added is ");
+  public static void main(String[] a) {
   }
 }
