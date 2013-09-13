e2fd69ef110a6ed69cc11aef1f9d828419a89916
mouli <mouli@eggheadcreative.com>
2013-09-13 23:46:41 +0530

    Completing NOP Instruction

--- src/CMDS_NOP.java
+++ src/CMDS_NOP.java
@@ -5,7 +5,8 @@
  * Submission date: 15th September, 2013
  */
 public class CMDS_NOP extends RISCInstruction {
-public static void exeCmd(RISCInstruction risc) {
-//  @TODO
-    }
-    }
+  public static void exeCmd(RISCInstruction risc) {
+//    This Instruction is useless and is expected to do nothing
+//    No commands are expected to be done here
+  }
+}
