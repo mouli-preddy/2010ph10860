3a38a53e317882d11df08e0f2547af3d776b0a4f
mouli <mouli@eggheadcreative.com>
2013-09-13 23:20:24 +0530

    Adding functions to help my memory remember my work ahead

---
+++ src/CMDS_B.java
@@ -0,0 +1,11 @@
+/**
+ * User: moulikrishna.
+ * Institute: IIT Delhi.
+ * purpose: As part of course requirements CSL211 Computer Architecture.
+ * Submission date: 15th September, 2013
+ */
+public class CMDS_B extends RISCInstruction {
+public static void exeCmd(RISCInstruction risc) {
+//  @TODO
+    }
+    }
