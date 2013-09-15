d193b99825d0b2423147a37168014e47beb83c75
mouli <mouli@eggheadcreative.com>
2013-09-15 10:57:21 +0530

    Filling out Stack

--- src/Stack.java
+++ src/Stack.java
@@ -5,4 +5,20 @@
  * Submission date: 15th September, 2013
  */
 public class Stack {
+
+  private int[] stack;
+  private int stackSize = 4100;
+  private int pointer;
+
+  public Stack() {
+    stack = new int[stackSize];
+    pointer = 0xFFF;
+  }
+
+  public int getPointer(int jump) {
+    int value = jump / 4;
+    if (value > 0) {
+      return value;
+    } else return 1;
+  }
 }
