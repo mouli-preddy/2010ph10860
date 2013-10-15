fe4cb7da20bed9129cfd8abf715ec55b41d0dd22
mouli <mouli@eggheadcreative.com>
2013-10-15 19:00:14 +0530

    Completed the function

--- src/CMDS_READ.java
+++ src/CMDS_READ.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 /**
  * User: moulikrishna.
  * Institute: IIT Delhi.
@@ -7,6 +9,25 @@
 public class CMDS_READ extends RISCInstruction {
 
   public static void exeCmd(RISCInstruction risc) {
-    //@TODO
+    //Create a string tokenizer with the input from Main.getNextLine();
+    String input = Main.readNextLine();
+    input.replace("0x", "");
+    StringTokenizer st = new StringTokenizer(input, " ");
+
+
+    //iterate through st and populate all the values.
+    int index = 0;
+    int address = Main.register.get(risc.getArg1());
+    while (st.hasMoreTokens()) {
+      //get the address to store value sp -
+      address--;
+
+      //get the value t be stored
+      int valueToStore = Integer.parseInt(st.nextToken(), 16);
+
+      //store value int the given address.
+      Main.stack.setValueAt(address, valueToStore);
+      index++;
+    }
   }
 }
